1d988cb6b2dbbc2ab79a74af3f15bed3486da08a
mylinhphxn <mylinhphxn2@outlook.com>
2022-07-07 09:52:02 -0700

    done

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -24,25 +24,38 @@
 */
 
 function coffeeTemplate(coffee){
-    
     let myReturn = "";
 
-    myReturn += `<p>
-    <img src="${coffee.src}" alt="${coffee.src}" id="coffee" />
-    <strong class="feature">${coffee.src}'s Coffee Special:</strong> ${coffee.src}'s daily coffee special is <strong class="feature">${coffee.src}</strong>, ${coffee.src}
-    </p>`;
+    myReturn += 
+    `
+        <p>
+            <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+           <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}
+        </p>
+    `;
 
     return myReturn;
-
 }
-
-
 
 
 let myDate = new Date();
 let myDay = myDate.getDay();
 let coffee = "";
 
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate querystring parameters
+const urlParams = new URLSearchParams(queryString);
+
+if(urlParams.has("day")){//data available? show data from url
+    myDay = urlParams.get("day");
+}
+//force to be an integer
+myDay = parseInt(myDay);
 
 switch(myDay){
 
@@ -70,10 +83,33 @@ switch(myDay){
 
   break;
 
+  case 4:
+    today = "Thursday";
+
+    coffee = {
+
+        color: "brown",
+        name: "Mocha",
+        pic: "images/mocha.jpg",
+        alt: "A picture of a Mocha",
+        day: "Thursday",
+        desc: `I love Mocha!`
+
+            };
+
+  break;
+
    default:
      today = "Something went wrong!";
 
 }
-coffeeData = coffeeTemplate(coffee);
-alert(coffee.pic);
-console.log(coffee);
+// coffeeData = coffeeTemplate(coffee);
+// alert(coffee.pic);
+
+// load coffee data to page 
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+document.querySelector("html").style.background = coffee.color;
+
+
+// console.log(coffee);
